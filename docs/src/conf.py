9c30298cef3c68cfc14bae4b82d69c818d06855f
Finn <finnryanit@gmail.com>
2024-12-06 16:29:34 +0000

    Please I beg you, just work

--- docs/src/conf.py
+++ docs/src/conf.py
@@ -24,7 +24,7 @@ extensions = [
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
-master_doc = 'src/index'
+master_doc = 'index'
 
 
 # -- Options for HTML output -------------------------------------------------
